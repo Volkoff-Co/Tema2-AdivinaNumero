56b54c3a48fead3f2cfee42d0d9e5c3bdc90e3b9
grigory volkov <gregoryvolkof@gmail.com>
2022-09-26 15:03:36 +0200

    Agregar archivos de proyecto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tema2_AdivinaNumero
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void comprobarButton_Click(object sender, RoutedEventArgs e)
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(101);
+            int num2 = Int32.Parse(textBox.Text.ToString());
+
+        }
+    }
+}
